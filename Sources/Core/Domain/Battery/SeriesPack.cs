ead6e9ec1c9d510cff1cc3b806261cf0cd18b7d4
Andrej Burger <burgera@impruvit.net>
2016-08-16 12:12:11 +0200

    Missing files added

---
+++ Sources/Core/Domain/Battery/SeriesPack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ImpruvIT.BatteryMonitor.Domain.Battery
+{
+	public class SeriesPack : Pack
+	{
+		public SeriesPack(IEnumerable<BatteryElement> subElements)
+			: base(subElements)
+		{
+		}
+	}
+}
